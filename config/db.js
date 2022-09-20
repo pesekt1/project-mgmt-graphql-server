fd3ebf4ab06ce24f77be2736391c4ac250794957
Tomas Pesek <pesekt@gmail.com>
2022-09-20 11:03:39 +0200

    refactoring

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI);
 
-  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+  console.log(`MongoDB Connected: ${conn.connection.host}`);
 };
 
 export default connectDB;
